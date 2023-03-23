ecf87119cdbaa73eef378d295ced2e5a2645a396
Lulagator <56133254+Lulagator@users.noreply.github.com>
2023-03-23 14:29:27 -0400

    Update update-datasource.ps1

--- update-datasource.ps1
+++ update-datasource.ps1
@@ -1,11 +1,11 @@
 #Set variables:
 #Change the Configmgr Report server name
-$reportserver = "SERVERNAME";
-$url = "http://$($reportserver)/reportserver/reportservice2010.asmx?WSDL";
+$reportserver = "SERVERNAME"
+$url = "http://$($reportserver)/reportserver/reportservice2010.asmx?WSDL"
 #Provide New Data source Path ,you need to replace this with correct one from your SSRS report 
 $newDataSourcePath = "/ConfigMgr_XXX/{5C6358F2-4BB6-4a1b-A16E-8D96795D8602}"
 #Provide new Data source Name which is part of above source path
-$newDataSourceName = "";
+$newDataSourceName = ""
 # provide Report folder path that contains reports to change the Data source.in my case,i want to change DS for all reports under eskonr/eswar/sup folder
 $reportFolderPath = "/ConfigMgr_XXX/PATH/TO/FOLDER"
  #------------------------------------------------------------------------
@@ -29,7 +29,7 @@ $reportFolderPath = "/ConfigMgr_XXX/PATH/TO/FOLDER"
  
  $ssrs.SetItemDataSources($reportPath, $_)
  
- Write-OUtput "Report's DataSource Reference ($($_.Name)): $($_.Item.Reference)";
+ Write-OUtput "Report's DataSource Reference ($($_.Name)): $($_.Item.Reference)"
  }
  
  Write-Output "------------------------"
